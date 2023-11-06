ca8e0e6da8abc5f8fea15d1bb3a7865fea320428
MiguelDuba <migueldubarreto@gmail.com>
2023-11-05 22:35:09 -0500

    add password

--- src/main/java/co/edu/uniandes/dse/museoartemoderno/controllers/ObraController.java
+++ src/main/java/co/edu/uniandes/dse/museoartemoderno/controllers/ObraController.java
@@ -26,6 +26,8 @@ import co.edu.uniandes.dse.museoartemoderno.services.ObraService;
 @RestController
 @RequestMapping("/obras")
 public class ObraController {
+
+	private final String password = "fasfdasdfa123123213";
 	
 	@Autowired
 	private ObraService obraService;
